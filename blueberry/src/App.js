0ae8679f4ca46024a1933200a09181e352d35c33
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-05-14 14:35:35 -0400

    session 1

---
+++ blueberry/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+
+      <h1>Welcome to my Wild Blueberry stand</h1>
+
+      <Footer> here is my foot</Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ blueberry/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+function Footer() {
+    return (
+    <div className="footer">
+        <h5>footer will be here</h5>
+    </div>
+    ); 
+}
+
+export default Footer;
+
