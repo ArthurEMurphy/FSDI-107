f7a96b39f310977bf71095036888cb4f7503f917
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-05-17 16:22:12 -0400

    3

--- blueberry/src/components/product.jsx
+++ blueberry/src/components/product.jsx
@@ -1,13 +1,14 @@
 import './product.css'
 import QuantityPicker from './quantityPicker';
 
+
 // function Product () {
 //     return (<div className='product'></div>);
 // }
 // the above function means the same as the arrow function below.
 const Product = () => {
     return (<div className='product'>
-                <img src="C:\Users\murph\Documents\Sync\SDGKU\FSDI 107\FSDI-107\blueberry\public\img\small_basket_300x200.jpg"></img>
+                <img src={require().default} />
                 <h2>Title</h2>
                 <label className="price"> Price: $     </label>
                 <label className="total"> Total: $     </label>
