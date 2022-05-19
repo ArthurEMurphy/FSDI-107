62fa44ccf95c4ce4d738f8fcb8e4b4344d4cb4dc
ArthurEMurphy <101611099+ArthurEMurphy@users.noreply.github.com>
2022-05-18 21:25:47 -0400

    13

--- blueberry/src/components/catalog.jsx
+++ blueberry/src/components/catalog.jsx
@@ -3,11 +3,12 @@ import Product from "./product";
 
 const Catalog = () => {
     return (<div className="catalog">
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
+        <h2>|   Our current blueberry offering   |</h2>
+        <Product title="Small Basket" price='10.00'></Product>
+        <Product title="Medium Basket" price='20.00'></Product>
+        <Product title="Large Basket" price='30.00'></Product>
+        <Product title="Blueberry Jam" price='8.00'></Product>
+        <Product title="Quart - Blueberry Gelato" price='13.00'></Product>
 
     </div>
     );
--- blueberry/src/components/product.jsx
+++ blueberry/src/components/product.jsx
@@ -6,16 +6,17 @@ import QuantityPicker from './quantityPicker';
 //     return (<div className='product'></div>);
 // }
 // the above function means the same as the arrow function below.
-const Product = () => {
+const Product = (props) => {
+    console.log('the parameter value is: '+String(props.title));
+
     return (<div className='product'>
-                <img src={require().default} />
-                <h2>Title</h2>
-                <label className="price"> Price: $     </label>
+                <img src="" />
+                <h2>{props.title}</h2>
+                <label className="price"> Price: ${props.price}</label>
                 <label className="total"> Total: $     </label>
                 <QuantityPicker></QuantityPicker>
                 <button>Add</button>
-                <button>Remove</button>
-
+                
             </div>);
 
 };
